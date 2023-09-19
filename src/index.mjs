a6af17a8517e4c0f47dc5345a0f0799f38fbbec6
kuuu83 <108081785+kuuu83@users.noreply.github.com>
2023-09-19 18:20:58 +0900

    Initial commit

---
+++ src/index.mjs
@@ -0,0 +1,15 @@
+/**
+ * const,let等の変数宣言
+ */
+
+//  var val1 = "varrrr1変数";
+//  console.log(val1);
+
+//  //var変数は上書き可能
+//  val1 = "上書きできちゃったよ";
+//  console.log(val1);
+
+// //var変数を再宣言
+// var val1 = "var変数を再宣言しちゃうよ"
+// console.log(val1);
+
